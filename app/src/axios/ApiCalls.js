9ba1ac85d07e44c4a79d73a2a03dd9003525b2a7
petardiplom <petar.diplomska@gmail.com>
2025-07-14 15:05:14 +0200

    archived services

--- app/src/axios/ApiCalls.js
+++ app/src/axios/ApiCalls.js
@@ -5,3 +5,8 @@ export const getServices = async () => {
     const response = await api.get('/services');
     return response.data;
 };
+
+export const getArchivedServices = async () => {
+    const response = await api.get('/services/archived');
+    return response.data;
+};
