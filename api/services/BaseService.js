697df2228ab19c28304c1668b4626acdf7b73f86
petardiplom <petar.diplomska@gmail.com>
2025-07-19 22:11:06 +0200

    withTransaction helper

--- api/services/BaseService.js
+++ api/services/BaseService.js
@@ -1,37 +1,72 @@
 import { buildInsertQuery, buildUpdateQuery } from "./utils.js";
 
 export class BaseService {
-    constructor(db) {
-        this.db = db;
+  constructor(db) {
+    this.db = db;
+  }
+
+  async withTransaction(callback, pool = this.db) {
+    const client = await pool.connect();
+    try {
+      await client.query("BEGIN");
+      const result = await callback(client);
+      await client.query("COMMIT");
+      return result;
+    } catch (err) {
+      await client.query("ROLLBACK");
+      throw err;
+    } finally {
+      client.release();
+    }
+  }
+
+  async findById(table, id) {
+    const result = await this.db.query(`SELECT * FROM ${table} WHERE id = $1`, [
+      id,
+    ]);
+    return result.rows[0] || null;
+  }
+
+  async findAllByField(table, field, value, ignoreArchived = false) {
+    let sql = `SELECT * FROM ${table} WHERE ${field} = $1 ORDER BY id ASC`;
+
+    if (ignoreArchived) {
+      sql = `SELECT * FROM ${table} WHERE ${field} = $1 AND archived_at IS NULL ORDER BY id ASC`;
     }
 
-    async findById(table, id) {
-        const result = await this.db.query(`SELECT * FROM ${table} WHERE id = $1`, [id]);
-        return result.rows[0] || null;
-    }
+    const result = await this.db.query(sql, [value]);
+    return result.rows;
+  }
 
-    async findAllByField(table, field, value, ignoreArchived = false) {
+  async insert(table, data, allowedFields, returning = "*") {
+    const { query, values } = buildInsertQuery(
+      table,
+      data,
+      allowedFields,
+      returning
+    );
 
-        let sql = `SELECT * FROM ${table} WHERE ${field} = $1 ORDER BY id ASC`
+    const result = await this.db.query(query, values);
+    return result.rows[0];
+  }
 
-        if(ignoreArchived){
-            sql = `SELECT * FROM ${table} WHERE ${field} = $1 AND archived_at IS NULL ORDER BY id ASC`
-        }
-        
-        const result = await this.db.query(sql, [value]);
-        return result.rows;
-    }
-
-    async insert(table, data, allowedFields, returning = '*') {
-        const { query, values } = buildInsertQuery(table, data, allowedFields, returning);
-
-        const result = await this.db.query(query, values);
-        return result.rows[0];
-    }
-
-    async update(table, data, allowedFields, whereClause, whereValues, returning = '*') {
-        const { query, values } = buildUpdateQuery(table, data, allowedFields, whereClause, whereValues, returning);
-        const result = await this.db.query(query, values);
-        return result.rows[0];
-    }
+  async update(
+    table,
+    data,
+    allowedFields,
+    whereClause,
+    whereValues,
+    returning = "*"
+  ) {
+    const { query, values } = buildUpdateQuery(
+      table,
+      data,
+      allowedFields,
+      whereClause,
+      whereValues,
+      returning
+    );
+    const result = await this.db.query(query, values);
+    return result.rows[0];
+  }
 }
