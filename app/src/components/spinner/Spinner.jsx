d0cf1ae8cec399add7875ff7b5052b27824f17da
petardiplom <petar.diplomska@gmail.com>
2025-08-05 13:58:05 +0200

    fix spinner component

--- app/src/components/spinner/Spinner.jsx
+++ app/src/components/spinner/Spinner.jsx
@@ -1,7 +1,7 @@
 import { CircularProgress } from "@mui/material";
 
 const Spinner = () => {
-  return <CircularProgress size="large" />;
+  return <CircularProgress size={40} />;
 };
 
 export default Spinner;
