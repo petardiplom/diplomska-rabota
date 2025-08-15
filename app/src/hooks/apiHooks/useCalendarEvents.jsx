2d8e2adf5ecf56d62f324253de97ce66993eced4
petardiplom <petar.diplomska@gmail.com>
2025-08-15 22:38:48 +0200

    calendar events prevent refetch on every date change, problem when traversing weeks, days

--- app/src/hooks/apiHooks/useCalendarEvents.jsx
+++ app/src/hooks/apiHooks/useCalendarEvents.jsx
@@ -1,14 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../../axios/axios";
 import { useCenter } from "../../contexts/CenterContext";
+import { startOfMonth } from "date-fns";
 
 export const CALENDAR_EVENTS_QUERY_KEY = "calendar_events";
 
 export const useCalendarEvents = (date, options = {}) => {
   const { centerId } = useCenter();
+  const monthKey = startOfMonth(date);
 
   return useQuery({
-    queryKey: [CALENDAR_EVENTS_QUERY_KEY, centerId, date],
+    queryKey: [CALENDAR_EVENTS_QUERY_KEY, centerId, monthKey],
     queryFn: async () => {
       const response = await api.get(`/calendar-events`, {
         params: { date },
